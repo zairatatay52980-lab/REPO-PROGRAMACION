450007769a4021893612f25e83d75a6fcd8fb26e
Zaira <zaira.tatay.52980@ikasle.egibide.org>
2025-12-15 10:56:28 +0100

    terminado/sin corregir

--- tema3pract3ej3/src/Main.java
+++ tema3pract3ej3/src/Main.java
@@ -16,27 +16,30 @@ public class Main {
     }
 
     public static void mostrarMenu() {
-        String opcion = JOptionPane.showInputDialog("--MENU PRINCIPAL--\n"
-                + "1. Añadir un libro a la pila\n"
-                + "2. Consultar libro\n"
-                + "3. REtirar libro\n"
-                + "4. Mostrar todos los libros\n"
-                + "5. Ver lista\n"
-                + "6. Salir");
-        hacerOpcion(opcion);
+        String opcion;
+       do {
+           opcion = JOptionPane.showInputDialog("--MENU PRINCIPAL--\n"
+                   + "1. Añadir un libro a la pila\n"
+                   + "2. Consultar libro\n"
+                   + "3. Retirar libro\n"
+                   + "4. Mostrar todos los libros\n"
+                   + "5. Ver lista\n"
+                   + "6. Salir");
+           hacerOpcion(opcion);
+       }while(!opcion.equalsIgnoreCase("6"));
     }
 
     public static void hacerOpcion(String opcion) {
         switch (opcion) {
             case "1": añadirLibro();
                 break;
-            case "2":
+            case "2":mostrarLibro();
                 break;
-            case "3":
+            case "3": eliminarLibro();
                 break;
-            case "4":
+            case "4":mostrarListaLibros();
                 break;
-            case "5":
+            case "5":verificarPilaVacia();
                 break;
             case "6":
                 break;
@@ -45,9 +48,33 @@ public class Main {
 
     public static void añadirLibro() {
         String titulo = JOptionPane.showInputDialog("Teclea el titulo del libro");
-        pilaLibros.add(titulo);
+        pilaLibros.push(titulo);
     }
 
+    public static void mostrarLibro() {
+        JOptionPane.showMessageDialog(null, pilaLibros.peek());
+    }
 
+    public static void eliminarLibro() {
+        JOptionPane.showMessageDialog(null, pilaLibros.pop());
 
+    }
+
+    public static void mostrarListaLibros() {
+        StringBuilder mostrarLista= new StringBuilder("Estos son los libros");
+
+        for(String libro:pilaLibros){
+            mostrarLista.append(":\n "+libro);
+        }
+
+        JOptionPane.showMessageDialog(null, mostrarLista);
+    }
+
+    public static void verificarPilaVacia() {
+        if (pilaLibros.isEmpty()) {
+            JOptionPane.showMessageDialog(null, "Pila esta vacia");
+        } else {
+            JOptionPane.showMessageDialog(null, "La pila no esta vacia?");
+        }
+    }
 }
