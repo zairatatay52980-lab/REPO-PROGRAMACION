973d15f8ce29a5a7a25dca88187913f4e281956d
Zaira <zaira.tatay.52980@ikasle.egibide.org>
2026-02-18 10:43:48 +0100

    Corrigiendo practica 1 ej2

--- TEMA 3/practica 1/prac1ej2/src/Main.java
+++ TEMA 3/practica 1/prac1ej2/src/Main.java
@@ -1,33 +1,45 @@
 import javax.swing.*;
+import java.util.Arrays;
 
 public class Main {
 
-    static double numeros[];
+    private static int[] numeros; // Declaración como global
 
     public static void main(String[] args) {
         try {
-            numeros=new int[Integer.parseInt(JOptionPane.showInputDialog("Teclea el numero de posiciones que deseas"))];
-            solicitarDatos();
-            mostrarDatos();
-        }catch(Exception e){
-            JOptionPane.showMessageDialog(null, "Problemas" + e.getMessage());
+            numeros = new int[Integer.parseInt(JOptionPane.showInputDialog("Indica el número de elementos"))];
+            llenado();
+            mostrar();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Problemas: " + e.getMessage());
         }
     }
-    public static void solicitarDatos(){
 
-    numeros = new double[dato];
-    for(int i=0;i<numeros.length;i++){
-        numeros[i]= (int) (Math.random()*10);
-    }
-    }
-    public static void mostrarDatos(){
-        int suma=0;
-        StringBuilder datos=new StringBuilder();
-        for(int i=0;i<numeros.length;i++){
-           datos.append("Posicion: ");
+    public static void llenado() throws Exception {
+        for (int x = 0; x < numeros.length; x++) {
+            numeros[x] = (int) (Math.random() * 10);
         }
-
-
     }
 
+    public static void mostrar() throws Exception {
+        int suma = 0;
+        StringBuilder datos = new StringBuilder();
+        for (int x = 0; x < numeros.length; x++) {
+            datos.append("Posición: ").append(x).append(" contenido: ").append(numeros[x]).append("\n");
+            suma += numeros[x];
+        }
+        JOptionPane.showMessageDialog(null, datos + " La suma asciende a: " + suma);
+
+        suma = 0;
+        for (int nro : numeros)
+            suma += nro;
+        JOptionPane.showMessageDialog(null, "La suma asciende a: " + suma);
+
+        suma = Arrays.stream(numeros).sum();
+        JOptionPane.showMessageDialog(null, "La suma asciende a: " + suma);
+
+        Arrays.stream(numeros)
+                .forEach(n -> System.out.println(n));
+
+    }
 }
